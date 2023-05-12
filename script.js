5cf40e385f19b314ccec2d205080aac65f3d22fd
Josenildo Costa <josenildocosta@gmail.com>
2023-05-12 12:02:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ function atualizarContador() {
   const agora = new Date();
   const diff = Math.floor((agora - dataInicio) / 1000); // Diferença em segundos
 
+  const dias = Math.floor((diff / 3600) / 24);
   const horas = Math.floor(diff / 3600);
   const minutos = Math.floor((diff % 3600) / 60);
   const segundos = diff % 60;
 
   // Formata a exibição do contador
   const contador = document.getElementById("contador");
-  contador.textContent = `${horas.toString().padStart(2, '0')}h ${minutos.toString().padStart(2, '0')}min ${segundos.toString().padStart(2, '0')}s`;
+  contador.textContent = `${dias.toString().padStart(2, '0')} dia ${horas.toString().padStart(2, '0')}h ${minutos.toString().padStart(2, '0')}min ${segundos.toString().padStart(2, '0')}s`;
 
   // Atualiza o contador a cada segundo
   setTimeout(atualizarContador, 1000);
